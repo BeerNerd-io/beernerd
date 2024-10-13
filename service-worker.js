d5a3a3206bf8affad3d25a7f19bd8f418eae50dc
beernerd.io <info@beernerd.io>
2024-10-13 16:34:24 -0500

    Restructure for GitHub Pages deployment

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = 'beer-nerd-labs-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/src/css/styles.css',
+    '/src/js/app.js',
+    '/src/components/task.js',
+];
+
+// Rest of the service worker code...
