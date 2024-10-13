f37bebf056b5af1cfef8d04d9a0075d5cad7e30a
beernerd.io <info@beernerd.io>
2024-10-13 18:49:55 -0500

    Update build scripts and file structure

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,37 @@
 const CACHE_NAME = 'beer-nerd-labs-v1';
 const urlsToCache = [
     '/',
-    '/index.html',
-    '/manifest.json',
-    '/src/css/styles.css',
-    '/src/js/app.js',
-    '/src/components/task.js',
-    '/path/to/beer-nerd-logo.png'
+    '/css/styles.min.css',
+    '/js/app.min.js',
+    '/images/beernerdgarage.png',
+    '/images/black.png'
 ];
 
-// Rest of the service worker code...
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.url.endsWith('index.html') || event.request.url.endsWith('/')) {
+        // Network-first strategy for index.html
+        event.respondWith(
+            fetch(event.request)
+                .catch(() => caches.match(event.request))
+        );
+    } else {
+        // Cache-first strategy for other resources
+        event.respondWith(
+            caches.match(event.request)
+                .then((response) => response || fetch(event.request))
+        );
+    }
+});
+
+// TODO: Add logic for updating cache and handling offline mode
+
+
+
+
